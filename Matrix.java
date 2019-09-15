c0bfb1c7b47b3cf6dbf79931899edec74b1f4ea6
mirzaalarsyad <mirza.fathan@gmail.com>
2019-09-15 23:16:02 +0700

    Ta,bahan OBE, serta Selektor sebaris

--- Matrix.java
+++ Matrix.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.io.*;
 
 public class Matrix {
 	private float[][] mTab;
@@ -35,6 +36,16 @@ public class Matrix {
 		return this.column;
 	}
 
+	public float[] entireRow(int m) {
+		float[] rowKe_m = new float[getColumn()];
+		for(int i=0; i<getColumn(); i++) {
+			rowKe_m[i] = this.mTab[m-1][i];
+		}
+		return rowKe_m;
+	}
+	/* m = {m | 1 <= m <= getRow(), m bilangan bulat}
+	untuk akses baris pertama gunakan entireRow(1) */
+
 	public void setSize() {
 		int m, n;
 		Scanner input = new Scanner(System.in);
@@ -44,7 +55,7 @@ public class Matrix {
 		this.column = n;
 	}
 
-	/* Input/Output */
+	/* Prosedur Input */
 	public void readMatrix() {
 		Scanner input = new Scanner(System.in);
 		setSize();
@@ -56,6 +67,72 @@ public class Matrix {
 			}
 		}
 	}
+
+	/* Operasi Baris Elementer */
+	public void swapRow(int x, int y) {
+		float[] temp1 = new float[getColumn()];
+		float[] temp2 = new float[getColumn()];
+
+		temp1 = entireRow(x);
+		temp2 = entireRow(y);
+
+		for(int i=0; i<getColumn(); i++) {
+			this.mTab[x-1][i] = temp2[i];
+			this.mTab[y-1][i] = temp1[i];
+		}
+	}
+	/* Operasi Menukar baris ke x dengan baris ke y dari mTab */
+
+	public void multiplyRow(int m, float num) {
+		for(int i=0; i<getColumn(); i++) {
+			this.mTab[m-1][i] *= num;
+		}
+	}
+	/* Operasi Perkalian Elemen Baris ke m dengan num  */
+
+	public void divideRow(int m, float num) {
+		for(int i=0; i<getColumn(); i++) {
+			this.mTab[m-1][i] /= num;
+		}
+	}
+	/* Operasi Pembagian Elemen Baris ke m dengan num */
+
+	public void addRows(int x, int y) {
+		float[] temp1 = new float[getColumn()];
+		float[] temp2 = new float[getColumn()];
+
+		temp1 = entireRow(x);
+		temp2 = entireRow(y);
+
+		for(int i=0; i<getColumn(); i++) {
+			this.mTab[x-1][i] = temp1[i] + temp2[i]; 
+		}
+	}
+	/* Operasi Pertambahan setiap Elemen pada baris ke-x dengan setiap
+	Elemen pada baris ke-y */
+
+	public void substractRows(int x, int y) {
+		float[] temp1 = new float[getColumn()];
+		float[] temp2 = new float[getColumn()];
+
+		temp1 = entireRow(x);
+		temp2 = entireRow(y);
+
+		for(int i=0; i<getColumn(); i++) {
+			this.mTab[x-1][i] = temp1[i] - temp2[i]; 
+		}
+	}
+	/* Operasi Pengurangan setiap Elemen pada baris ke-x dengan setiap
+	Elemen pada baris ke-y */
+
+	/* Input dan Output */
+
+	/*
+	public void filereadMatrix() {
+		File fileInput = new File("in.txt");
+		Scanner fileInput = new Scanner(fileInput)
+
+	}*/
 
 	public void printMatrix() {
 		int m = getRow();
